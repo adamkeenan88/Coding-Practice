8d6f1660c0cfe251d37ed2ba25c0fa1b119c31bd
admin <admin@admins-MacBook-Pro.local>
2021-10-17 20:09:00 -0500

    starting server doc

--- message_board_practice/server/server.js
+++ message_board_practice/server/server.js
@@ -1,2 +1,33 @@
-import Mongoose from "mongoose";
-import Express from "express";
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const socket = require("socket.io");
+
+require("./config/mongoose.config");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require("./server/routes/pet.routes")(app);
+const server = app.listen(8000, () => {
+  console.log("Listening at Port 8000");
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT"],
+    allowedHeaders: ["*"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("socket id: " + socket.id);
+
+  socket.on("event_from_client", (data) => {
+    // send a message with "data" to ALL clients EXCEPT for the one that emitted the
+    //     "event_from_client" event
+    socket.broadcast.emit("event_to_all_other_clients", data);
+  });
+});
