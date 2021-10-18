18dbacb33785199475ce46bb97f50a04df6b5a27
admin <admin@admins-MacBook-Pro.local>
2021-10-17 21:04:43 -0500

    server setup

--- message_board_practice/server/server.js
+++ message_board_practice/server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-require("./server/routes/pet.routes")(app);
+require("./routes/cars.routes")(app);
 const server = app.listen(8000, () => {
   console.log("Listening at Port 8000");
 });
